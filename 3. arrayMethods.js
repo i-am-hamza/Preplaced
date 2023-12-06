e5efc2c2e69671ac9837708047b216bdbfe94e08
Hamza <54952184+i-am-hamza@users.noreply.github.com>
2023-12-07 00:36:08 +0530

    Array Methods

--- 3. arrayMethods.js
+++ 3. arrayMethods.js
@@ -43,6 +43,44 @@ console.log(cars);          // [ 'Audi', 'Ferrari', 'Tesla', <1 empty item>, 'Be
 console.log(cars[3]);       // undefined
 
 
+cars[3] = 'Nissan';
+
+// concat - merge two arrays and creates a new array
+
+const bikes = ['Fz', 'Harley', 'Royal Enfield']
+const vehicles = cars.concat(bikes);
+console.log("Cars are: ", cars);
+console.log("Vehicles are: ", vehicles);
 
 
+// flat - it reduces the dimensity of the array
+const myArr = [[1,2],[2,4],[3,4]];
+const newArr = myArr.flat();
 
+console.log(myArr);
+console.log(newArr);
+
+
+// map - creates a new array from an existing array by applying a function to each of the elements.
+const array = [1, 2, 3, 4, 5, 6, 7, 8]
+const newArrMap = array.map((num, i, array)=>{
+    return num * num;
+})
+
+console.log(newArrMap);
+
+
+// filter - creates a new array by applying a conditional statement on each element of the exiting array
+const newArrFilter = array.filter((num, i, array)=>{
+    return num > 4;
+})
+
+console.log(newArrFilter);
+
+
+// reduce - creates a new array by reducing the existing array by applying function to each element and accumulating the result
+const sumReduce = array.reduce((prev, curr, i, array)=>{
+    return prev + curr;
+}, 0)
+
+console.log(sumReduce);
