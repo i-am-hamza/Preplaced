b4f2f67f0811d5438e31d1225789eadecc598b13
Hamza <54952184+i-am-hamza@users.noreply.github.com>
2024-01-03 23:51:33 +0530

    Practice

---
+++ 9. Synchronous.js
@@ -0,0 +1,49 @@
+// 1. Comparison Operator
+
+var a = "42";
+var b = 42
+
+console.log(a == b); // true
+console.log(a === b); // false
+
+
+// 2. Write a JavaScript program to display the current day and time in the following format.
+
+var today = new Date();
+console.log(today);
+
+var date = today.getDate()
+
+var dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+console.log("Today is", dayList[date], ".");
+
+var hour = today.getHours();
+var minutes = today.getMinutes();
+var seconds = today.getSeconds();
+
+var prepand = (hour >= 12)? "PM":"AM";
+
+hour = (hour >=12)? hour - 12: hour;
+
+if (hour === 0 && prepand === "PM"){
+    if (minute === 0 && second === 0){
+        hour = 12;
+        prepand = 'Noon';
+    }else{
+        hour = 12;
+        prepand = 'PM';
+    }
+}
+
+if (hour === 0 && prepand === 'AM'){
+    if(minute === 0 && second === 0){
+        hour = 0;
+        prepand = 'Midnight'
+    }else{
+        hour = 12;
+        prepand = 'AM'
+    }
+}
+
+console.log("Time is", hour + prepand + " : " + minute +  + seconds);
